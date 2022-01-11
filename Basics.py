ea096de4f9cfa0a35567bc6b448fc5aa5517a495
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-01-11 21:05:22 +0530

    11/01/2022 at 09:05 PM

--- Basics.py
+++ Basics.py
@@ -17,7 +17,7 @@ days=rem_age*365
 weeks=rem_age*52
 months=rem_age*12
 print(f"You have {days} days,{weeks} weeks and {months} months left")'''
-print("Welcome to tip calculater")
+'''print("Welcome to tip calculater")
 bill=float(input("What was the total bill?"))
 tip=int(input("What percentage of tip would you like to give?10,12 or 15?"))
 people=int(input("How many people to split the bill?"))
@@ -25,4 +25,22 @@ add_tip=bill*(tip/100)
 total_bill=bill+add_tip
 per_person=total_bill/people
 
-print(f"Each person should pay{round(per_person,2)}")
+print(f"Each person should pay{round(per_person,2)}")'''
+print("Welcome to True Love Calculator")
+name1=input("Enter ur Name: ")
+name2=input("Enter Their Name: ")
+lower_name1=name1.lower()
+lower_name2=name2.lower()
+com_str=lower_name1+lower_name2
+print(com_str)
+t=com_str.count("t")
+r=com_str.count("r")
+u=com_str.count("u")
+e=com_str.count("e")
+true=t+r+u+e
+l=com_str.count("l")
+o=com_str.count("o")
+v=com_str.count("v")
+e=com_str.count("e")
+love=l+o+v+e
+print(str(true)+str(love))
