36fe9788644e49cc46d98b7e577f647177bb69e4
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-01-11 14:58:12 +0530

    11/01/2022 at 02:58 PM

--- Basics.py
+++ Basics.py
@@ -1,4 +1,11 @@
 print("Nothing to say")
 print('No thing to say')
-print("OK!"+input("Ur Name"))
-print(len(input("Age!")))
+#print("OK!"+input("Ur Name"))
+#print(len(input("Age!")))
+print("Hello"[3])
+num=input("Enter The number")
+num1=num[0]
+num2=num[1]
+num1=int(num1)
+num2=int(num2)
+print(num1+num2)
