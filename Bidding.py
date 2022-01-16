0cdf66a8cbcf9a87eb1c33e3d2f5072565b838c6
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-01-16 22:56:30 +0530

    16/01/2022 at 10:56 PM

---
+++ Bidding.py
@@ -0,0 +1,23 @@
+from turtle import clear
+
+
+bids={}
+def highest(record):
+    high=0
+    winner=""
+    for i in record:
+        amount=record[i]
+        if amount > high:
+            high=amount
+            winner=i
+    print(f"The winner is{winner} with Amount of{high}")        
+name=input("Enter The Name")
+value=int(input("Enter The Price"))
+bidding_finished= False
+while not bidding_finished:
+    bids[name]=value
+    extra=input("Are There Any Bidders To add? y or n")
+    if extra=="n":
+     bidding_finished=True
+    elif extra=="y":
+      clear()
